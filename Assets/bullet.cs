64f4a9e1d8ea0868e98ba0b288aed57fdba21cc0
jongcheonpark2 <jongcheonpark2@users.noreply.github.com>
2020-06-18 16:04:42 +0900

    6월 18일 박종천

---
+++ Assets/bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody bulletRigidbody;
+
+    void Start()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+        bulletRigidbody.velocity = transform.forward * speed;
+        Destroy(gameObject, 3f);
+    }
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerContorller playerController = other.GetComponent<PlayerContorller>();
+            if (playerController != null)
+            {
+                playerController.Die();
+            }
+        }
+    }
+}
